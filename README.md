7b1e6ccd337180fc498419e46cc7ed1aca48fe39
venky252003 <venky25@gmail.com>
2019-11-06 20:42:22 +0530

    test API endpoints

--- README.md
+++ README.md
@@ -26,8 +26,21 @@ Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protrac
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
 
-##Git
+## Git
 
 git commit -m "Agular Code"
+
 git remote add origin https://github.com/venky252003/Angular8_App.git
+
 git push -u origin dev
+
+## Heroku
+
+Login : heroku login
+
+Create Heroku project link : heroku create
+
+https://morning-wave-77600.herokuapp.com/ | https://git.heroku.com/morning-wave-77600.git
+
+Create MangoDB Connection: heroku addons:create mongolab
+
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,8 @@ import { Routes, RouterModule } from '@angular/router';
 import { ErrorComponent } from './error.component';
 
 const routes: Routes = [
-    { path: 'customers',  loadChildren: () => import('./customers/customer.module').then(mod => mod.CustomerModule)},    
-    { path: 'products', loadChildren: () => import('./product/product.module').then(mod => mod.ProductModule) },
+    { path: 'customers',  loadChildren: () => import(`./customers/customer.module`).then(mod => mod.CustomerModule)},    
+    { path: 'products', loadChildren: () => import(`./product/product.module`).then(mod => mod.ProductModule) },
     { path: 'about', loadChildren: () => import('./about/about.module').then(mod => mod.AboutModule) },
     { path: 'contact', loadChildren: () => import('./contact/contact.module').then(mod => mod.ContactModule) },
     { path: '',   redirectTo: '/customers', pathMatch: 'full' },
